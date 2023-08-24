f98d6139fbc6c03053e34cab5d65effdb8de5957
Pramod Jay <91390000+pramod-jay@users.noreply.github.com>
2023-08-24 23:44:03 +0530

    Connect Gateway

--- Gateway/index.js
+++ Gateway/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const app = express();
-const functions = require('./routes/routes');
+// const functions = require('./routes/routes');
+const proxy = require('express-http-proxy');
 
 dotenv.config();
 
 app.use(express.json());
+app.use(cors());
 
-app.use('/', functions);
+app.use('/user', proxy('http://localhost:8001')); //User endpoint
+app.use('/order', proxy('http://localhost:8002'));
+app.use('/inventory', proxy('http://localhost:8003'));
+
+// app.use('/', functions);
 
 app.listen(process.env.PORT, () => {
     console.log('Server run on port: ', process.env.PORT);

1f11f2433218b96659adb5fb0b5bea4c5f42622a
Nathali Fernando <99954012+NathaliFdo@users.noreply.github.com>
2023-09-05 21:22:16 +0530

    User Finalized

--- User/service/connection.js
+++ User/service/connection.js
@@ -19,21 +19,5 @@ connection.connect(function(err){
 module.exports = connection
 
 
-// var mysql = require('mysql');
 
-// var connection=mysql.createConnection({
-//     host:"user-server.mysql.database.azure.com", 
-//     user:"VikingsAdmin", 
-//     password:"VAB20@1234", 
-//     database:"user", 
-//     port:3306, 
-//     // ssl:{ca:fs.readFileSync("{ca-cert filename}")}
-// });
-
-// connection.connect(function (err) {
-//     if(err) throw err;
-//     console.log("connected to User database");
-// });
-
-// module.exports = connection
 
